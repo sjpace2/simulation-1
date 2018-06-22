a5c5240444b4a6d6e8ebcc05f095110b0742812c
Stephen Pace <stephen@Stephens-MacBook-Pro.local>
2018-06-22 10:07:47 -0600

    some initial setup

---
+++ shelfie/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Dashboard extends Component {
+    constructor () {
+        super()
+    }
+
+    render(){
+        return(
+            <div>
+                Dashboard
+            </div>
+        )
+    }
+}
+
+export default Dashboard;
